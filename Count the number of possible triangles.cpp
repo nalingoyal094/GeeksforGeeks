d4c4c4cd885f119175b043581da06d8928b73109
nalingoyal094 <52338664+nalingoyal094@users.noreply.github.com>
2021-06-07 00:34:40 +0530

    Create Count the number of possible triangles.cpp

---
+++ Count the number of possible triangles.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+    public:
+    int findNumberOfTriangles(int arr[], int n)
+    {
+        sort(arr, arr+n);
+        
+        int count = 0;
+        
+        for(int c = n-1; c >= 2; c--)
+        {
+            int a = 0, b = c - 1;
+            
+            while(a < b)
+            {
+                if(arr[a] + arr[b] > arr[c])
+                {
+                    count += b - a;
+                    
+                    b--;
+                }
+                else
+                {
+                    a++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
+
+//Expected Time Complexity: O(n2).
+// Space Complexity: O(1).
