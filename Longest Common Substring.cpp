ed58169a11c5055664d2a195f9b554f466adabfa
nalingoyal094 <52338664+nalingoyal094@users.noreply.github.com>
2021-06-27 23:11:45 +0530

    Create Longest Common Substring.cpp

---
+++ Longest Common Substring.cpp
@@ -0,0 +1,32 @@
+int longestCommonSubstr (string S1, string S2, int n, int m)
+    {
+        int dp[n+1][m+1];
+        int ma=0;
+        for (int i=0;i<=n;i++)
+        {
+            for (int j=0;j<=m;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                
+                
+                if (S1[i-1]==S2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                    ma=max(ma,dp[i][j]);
+                }
+                else
+                {
+                    dp[i][j]=0;
+                }
+                
+                
+            }
+        }
+        return ma;
+        
+        
+    }
